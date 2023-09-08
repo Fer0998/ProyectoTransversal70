10f7287192fd13c3feee61d74fe9a5e2a98e54d1
barbaraponce93 <barbaraponce93@gmail.com>
2023-09-08 11:41:04 -0300

    Inscripcion

--- src/proyectotransversal70/entidades/Inscripcion.java
+++ src/proyectotransversal70/entidades/Inscripcion.java
@@ -4,4 +4,65 @@ package proyectotransversal70.entidades;
 
 public class Inscripcion {
     
+    private int idInscripto;
+    private double nota;
+    private Alumno idAlumno;
+    private Materia IDMateria;
+
+    public Inscripcion(int idInscripto, double nota, Alumno idAlumno, Materia IDMateria) {
+        this.idInscripto = idInscripto;
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.IDMateria = IDMateria;
+    }
+
+    public Inscripcion(double nota, Alumno idAlumno, Materia IDMateria) {
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.IDMateria = IDMateria;
+    }
+
+    public Inscripcion() {
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(Alumno idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public Materia getIDMateria() {
+        return IDMateria;
+    }
+
+    public void setIDMateria(Materia IDMateria) {
+        this.IDMateria = IDMateria;
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" + "idInscripto=" + idInscripto + ", nota=" + nota + ", idAlumno=" + idAlumno + ", IDMateria=" + IDMateria + '}';
+    }
+    
+    
+    
+    
 }
