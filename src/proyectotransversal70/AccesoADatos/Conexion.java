64e01817676a4f1fde9e85e23814314b0a3747f4
barbaraponce93 <barbaraponce93@gmail.com>
2023-09-08 12:23:24 -0300

    clase conexion completa

--- src/proyectotransversal70/AccesoADatos/Conexion.java
+++ src/proyectotransversal70/AccesoADatos/Conexion.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectotransversal70.AccesoADatos;
 
-/**
- *
- * @author Pc
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="unlp";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
     
-}
+    private Conexion(){
+        
+    }
+    
+    public static Connection getConexion(){
+        if(connection==null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection=DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                
+                JOptionPane.showMessageDialog(null, "Base de datos connectado");
+                
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "No se cargaron los drivers");
+            } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "No se pudo conectar con la Base de datos");
+            }
+             
+        }
+   return connection;
+       
+    
+    }
+}//fin clase
