37f64a5c46e257c5714562d196dac4baa8f1c236
Fer0998 <estebanfernandoalvarez@gmail.com>
2023-09-08 11:18:54 -0300

    clase alumno( entidades)
    Ya complete la clase alumnos del paquete entidades

--- src/proyectotransversal70/entidades/Alumno.java
+++ src/proyectotransversal70/entidades/Alumno.java
@@ -1,14 +1,99 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectotransversal70.entidades;
 
-/**
- *
- * @author fernandoalvarez
- */
+import java.time.LocalDate;
+
+
 public class Alumno {
+      private int id;
+    private int dni;
+    private String apellido;
+    private String nombre;
+    private LocalDate fNacimiento;
+    private boolean estado;
+//-----------------------------------------------------------------------------------------------------------
+    public Alumno(int id, int dni, String apellido, String nombre, LocalDate fNacimiento, boolean estado) {
+        this.id = id;
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fNacimiento = fNacimiento;
+        this.estado = estado;
+    }
+//-----------------------------------------------------------------------------------------------------------
+    public Alumno(int dni, String apellido, String nombre, LocalDate fNacimiento, boolean estado) {
+        
+        this.dni = dni;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.fNacimiento = fNacimiento;
+        this.estado = estado;
+    }
+//-----------------------------------------------------------------------------------------------------------
+    public Alumno(){}
     
-}
+//-----------------------------------------------------------------------------------------------------------
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getfNacimiento() {
+        return fNacimiento;
+    }
+
+    public void setfNacimiento(LocalDate fNacimiento) {
+        this.fNacimiento = fNacimiento;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+//-----------------------------------------------------------------------------------------------------------
+    @Override
+    public String toString() {
+         return "Alumno\n"+
+                "-------------------------"+"\n"+
+                "Id       = " + id +"\n"+ 
+                "Dni      = " + dni +"\n"+ 
+                "Apellido = " + apellido +"\n"+ 
+                "Nombre   = " + nombre + "\n"+
+                "Fecha Na = " + fNacimiento +"\n"+ 
+                "Estado   = " + estado +"\n"+
+                "-------------------------";
+    }
+    
+}// fin class
