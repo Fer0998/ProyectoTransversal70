0ada3a3fac7f3ce75dac5a518c39107061d37185
Fer0998 <estebanfernandoalvarez@gmail.com>
2023-09-15 10:52:28 -0300

    GestionAlumno
    Tabla de gestion alumno configurada pero sin terminar...

--- src/proyectotransversal70/vistas/MenuPrincipal.java
+++ src/proyectotransversal70/vistas/MenuPrincipal.java
@@ -50,6 +50,11 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jMenu2.setText("Alumno");
 
         jMenuItem1.setText("Formulario de Alumno");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem1);
 
         jMenuBar1.add(jMenu2);
@@ -96,6 +101,17 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+       
+        escritorio.removeAll();
+        escritorio.repaint();
+        gestionAlumno ventanaAlumno = new gestionAlumno();
+        ventanaAlumno.setVisible(true);   
+        escritorio.add(ventanaAlumno);
+        escritorio.moveToFront(ventanaAlumno);
+  
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     /**
      * @param args the command line arguments
